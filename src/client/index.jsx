1344d65cfb7d8f7d74e789c118cab62559c31c2c
Bulat <gink@MacBook-Air-Bulat.local>
2023-03-04 18:55:29 +0300

    replace render => createRoot

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from "./containers/app.jsx";
 import './styles.scss';
 
-ReactDom.render((
-  <Provider store={store}>
-    <App message='hi'/>
-  </Provider>
-), document.getElementById('tetris'));
+const root = ReactDom.createRoot(document.getElementById("tetris"));
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
